9206022ec48c617bbcae4b52255c518740617c66
tammytamayao <tammytamayaogmail.com@Princesss-MacBook-Pro.local>
2024-11-02 15:17:39 +0800

    feat: add pagination feature

--- src/app/api.tsx
+++ src/app/api.tsx
@@ -5,12 +5,21 @@ export interface Expense {
   title: string;
   description: string;
   amount: number;
+  date: Date;
 }
 
 type NewExpense = Omit<Expense, "id">;
 
-export const fetchExpenses = async (): Promise<Expense[]> => {
-  const response = await fetch(`${API_URL}/expenses`);
+export const fetchExpenses = async (
+  page: number
+): Promise<{
+  expenses: Expense[];
+  total_amount: number;
+  current_page: number;
+  per_page: number;
+  total_pages: number;
+}> => {
+  const response = await fetch(`${API_URL}/expenses?page=${page}`);
   if (!response.ok) {
     throw new Error("Failed to fetch expenses");
   }
