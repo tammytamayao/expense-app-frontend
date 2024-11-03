96cb5991e84f68ab9ab16d276da00c403c3dc76c
tammytamayao <tammytamayaogmail.com@Princesss-MacBook-Pro.local>
2024-11-03 22:18:23 +0800

    refactor: add and edit expense page

--- src/app/expenses/add/page.tsx
+++ src/app/expenses/add/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ExpenseForm from "../components/ExpenseForm";
-import MessageDisplay from "../../components/MessageDisplay";
 import { addExpense } from "../../api";
-import Header from "@/app/components/Header";
+import ExpenseFormContainer from "../components/ExpenseFormContainer";
 
 interface Expense {
   id?: number;
@@ -21,64 +20,31 @@ const AddExpensePage: React.FC = () => {
   const [message, setMessage] = useState<string | null>(null);
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
 
-  const username = sessionStorage.getItem("username");
-
-  useEffect(() => {
-    if (!username) {
-      router.push("/");
-    }
-  }, [username]);
-
   const handleAdd = async (expense: Expense) => {
-    if (!username) {
-      setMessage("User not found. Please log in again.");
-      setIsSuccess(false);
-      return;
-    }
-
-    const expenseData = {
-      ...expense,
-      username,
-    };
-
     try {
-      await addExpense(expenseData);
+      await addExpense(expense);
       setMessage("Expense added successfully!");
       setIsSuccess(true);
       setTimeout(() => router.push("/expenses"), 1000);
-    } catch (error) {
+    } catch {
       setMessage("Failed to add expense. Please try again.");
       setIsSuccess(false);
-      setTimeout(() => {
-        setMessage(null);
-      }, 1000);
+      setTimeout(() => setMessage(null), 1000);
     }
   };
 
   return (
-    <div>
-      <Header />
-      <div className="flex flex-col h-screen items-center justify-center bg-gray-100">
-        <div className="max-w-xl w-full items-center justify-center bg-gray-100">
-          {message && (
-            <MessageDisplay message={message} isSuccess={isSuccess} />
-          )}
-        </div>
-        <div className="bg-white p-8 rounded-lg shadow-md text-left max-w-xl w-full">
-          <div className="flex justify-between items-center mb-4">
-            <h1 className="text-3xl font-bold text-gray-800">Add Expense</h1>
-            {/* Back button */}
-            <button
-              onClick={() => router.back()}
-              className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-800 transition-colors"
-            >
-              <strong>{">"}</strong>
-            </button>
-          </div>
-          <ExpenseForm onSubmit={handleAdd} isEdit={false} />
-        </div>
-      </div>
-    </div>
+    <ExpenseFormContainer
+      title="Add Expense"
+      isEdit={false}
+      onSubmit={handleAdd}
+      message={message}
+      setMessage={setMessage}
+      isSuccess={isSuccess}
+      setIsSuccess={setIsSuccess}
+    >
+      <ExpenseForm onSubmit={handleAdd} isEdit={false} />
+    </ExpenseFormContainer>
   );
 };
 
